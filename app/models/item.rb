49bda39dc9fdfa7a99d52b35578025081f5bea41
Damir-Che <chechuhov.damir@mail.ru>
2020-08-30 23:01:37 +0300

    has_and_belongs_to_many

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   # , allow_nil: true - цена может быть пустой
   validates :price, { numericality: { greater_than: 0, allow_nil: true } }
   validates :name, :description, presence: true
+
+  has_and_belongs_to_many :carts
 end
